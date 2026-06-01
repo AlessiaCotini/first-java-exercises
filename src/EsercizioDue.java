0c51b8c05a17bb3e14c78e4565e1d469a3702fd7
AlessiaCotini <alessia.cotini@icloud.com>
2026-06-01 14:45:37 +0200

    ex 2

--- src/EsercizioDue.java
+++ src/EsercizioDue.java
@@ -1,3 +1,15 @@
-public class EsercizioDue {
+import java.util.Scanner;
 
+public class EsercizioDue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserire prima parola");
+        String prima = scanner.nextLine();
+        System.out.println("inserire seconda parola");
+        String seconda = scanner.nextLine();
+        System.out.println("inserire terza parola");
+        String terza = scanner.nextLine();
+        System.out.println(prima +" "+ seconda +" "+terza);
+        System.out.println(terza +" "+ seconda +" "+prima);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 import java.util.Arrays;
 
 public class Main {
-public static void main(String[] args){
+public static void main(String[] args) {
     System.out.println("Hello");
- EsercizioUno esempio = new EsercizioUno();
- int prodotto = esempio.moltiplica(5,6);
+    EsercizioUno esempio = new EsercizioUno();
+    int prodotto = esempio.moltiplica(5, 6);
     System.out.println(prodotto);
 
 
- String concatenata = esempio.concatena("ciao", 5);
+    String concatenata = esempio.concatena("ciao", 5);
     System.out.println(concatenata);
 
 
- String[] lista = {"mela", "arancia", "susina","banana","fragola"};
- String[] inserisci = esempio.InserisciInArray(lista, "pera");
+    String[] lista = {"mela", "arancia", "susina", "banana", "fragola"};
+    String[] inserisci = esempio.InserisciInArray(lista, "pera");
     System.out.println(Arrays.toString(inserisci));
-
 }
 }
